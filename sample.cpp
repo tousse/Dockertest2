2b0d2b29d5dd466ab8537758d1a2522eba76ea97
tousse <36902118+tousse@users.noreply.github.com>
2019-09-25 15:02:49 +0100

    Add files via upload

--- sample.cpp
+++ sample.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[ ])
 
 QApplication app(argc, argv);
 QLabel hello("<center>Welcome to my first Qt program</center>");
-hello.setWindowTitle("My First Qt Program");
+hello.setWindowTitle("My First Qt Program!Noice!!!");
 hello.resize(400, 400);
 hello.show();
 return app.exec();
